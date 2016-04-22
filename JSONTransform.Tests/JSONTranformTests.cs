27203f394a5affa5e32085c28094ed894e4b2dad
Gareth <gareth.marland@gmail.com>
2016-04-21 22:23:07 -0400

    Add project files.

---
+++ JSONTransform.Tests/JSONTranformTests.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using JSONTranform;
+using System.IO;
+
+namespace JSONTranform.Tests
+{
+    [TestClass]
+    public class TransformationHelperTests
+    {
+        // Transform JSON tests
+
+        [TestMethod]
+        public void TransformSimpleJSONValid()
+        {
+            JToken source = GetJSONObject(@"JSON\Source\simple.json");
+            JToken transform = GetJSONObject(@"JSON\Transform\simple.json");
+
+            JToken result = Transformer.TransformJSON(source, transform);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(JArray));
+
+            JArray resultArray = ((JArray)result);
+
+            Assert.AreEqual(resultArray.Count, 3);
+
+            foreach (JToken entry in resultArray.Children())
+            {
+                Assert.IsInstanceOfType(entry, typeof(JObject));
+
+                JObject child = (JObject)entry;
+
+                if (child["responseType"] != null) Assert.AreEqual("survey", (string)child["responseType"]);
+                else if (child["item"] != null)
+                {
+                    if (((string)child["item"]) == "question") Assert.AreEqual("How are you doing?", (string)child["text"]);
+                    else if (((string)child["item"]) == "answer") Assert.AreEqual("I'm doing pretty well", (string)child["text"]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TransformNestedJSONValid()
+        {
+            JToken source = GetJSONObject(@"JSON\Source\nested.json");
+            JToken transform = GetJSONObject(@"JSON\Transform\nested.json");
+
+            JToken result = Transformer.TransformJSON(source, transform);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(JArray));
+
+            JArray resultArray = ((JArray)result);
+
+            Assert.AreEqual(resultArray.Count, 3);
+
+            foreach (JToken entry in resultArray.Children())
+            {
+                Assert.IsInstanceOfType(entry, typeof(JObject));
+
+                JObject child = (JObject)entry;
+
+                if (child["responseType"] != null) Assert.AreEqual("survey", (string)child["responseType"]);
+                else if (child["item"] != null)
+                {
+                    if (((string)child["item"]) == "question") Assert.AreEqual("How are you doing?", (string)child["text"]);
+                    else if (((string)child["item"]) == "answer") Assert.AreEqual("I'm doing pretty well", (string)child["text"]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TransformArrayJSONValid()
+        {
+            JToken source = GetJSONObject(@"JSON\Source\array.json");
+            JToken transform = GetJSONObject(@"JSON\Transform\array.json");
+
+            JToken result = Transformer.TransformJSON(source, transform);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(JArray));
+
+            JArray resultArray = ((JArray)result);
+
+            Assert.AreEqual(resultArray.Count, 3);
+
+            foreach (JToken entry in resultArray.Children())
+            {
+                Assert.IsInstanceOfType(entry, typeof(JObject));
+
+                JObject child = (JObject)entry;
+
+                if (child["responseType"] != null) Assert.AreEqual("survey", (string)child["responseType"]);
+                else if (child["item"] != null)
+                {
+                    if (((string)child["item"]) == "question") Assert.AreEqual("How are you doing?", (string)child["text"]);
+                    else if (((string)child["item"]) == "answer") Assert.AreEqual("I'm doing pretty well", (string)child["text"]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TransformArrayWholeJSONValid()
+        {
+            JToken source = GetJSONObject(@"JSON\Source\array.json");
+            JToken transform = GetJSONObject(@"JSON\Transform\simple.json");
+
+            JToken result = Transformer.TransformJSON(source, transform);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(JArray));
+
+            JArray resultArray = ((JArray)result);
+
+            Assert.AreEqual(resultArray.Count, 3);
+
+            foreach (JToken entry in resultArray.Children())
+            {
+                Assert.IsInstanceOfType(entry, typeof(JObject));
+
+                JObject child = (JObject)entry;
+
+                if (child["responseType"] != null) Assert.AreEqual("survey", (string)child["responseType"]);
+                else if (child["item"] != null)
+                {
+                    if (((string)child["item"]) == "question") Assert.AreEqual("How are you doing?", (string)child["text"]);
+                    else if (((string)child["item"]) == "answer")
+                    {
+                        JArray childArray = (JArray)child["text"];
+
+                        Assert.AreEqual(2, childArray.Count);
+                        Assert.AreEqual("I'm doing pretty well", childArray[0]);
+                        Assert.AreEqual("I wish I was better", childArray[1]);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TransformNestedArrayJSONValid()
+        {
+            JToken source = GetJSONObject(@"JSON\Source\nestedArray.json");
+            JToken transform = GetJSONObject(@"JSON\Transform\arrayNested.json");
+
+            JToken result = Transformer.TransformJSON(source, transform);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(JArray));
+
+            JArray resultArray = ((JArray)result);
+
+            Assert.AreEqual(resultArray.Count, 3);
+
+            foreach (JToken entry in resultArray.Children())
+            {
+                Assert.IsInstanceOfType(entry, typeof(JObject));
+
+                JObject child = (JObject)entry;
+
+                if (child["responseType"] != null) Assert.AreEqual("survey", (string)child["responseType"]);
+                else if (child["item"] != null)
+                {
+                    if (((string)child["item"]) == "question") Assert.AreEqual("How are you doing?", (string)child["text"]);
+                    else if (((string)child["item"]) == "answer") Assert.AreEqual("A different response", (string)child["text"]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TransformArrayNestedJSONValid()
+        {
+            JToken source = GetJSONObject(@"JSON\Source\nestedNested.json");
+            JToken transform = GetJSONObject(@"JSON\Transform\nestedArray.json");
+
+            JToken result = Transformer.TransformJSON(source, transform);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(JObject));
+
+            JObject resultObject = ((JObject)result);
+
+            Assert.AreEqual("survey", (string)resultObject["responseType"]);
+
+            Assert.IsInstanceOfType(resultObject["items"], typeof(JArray));
+
+            JArray itemsArray = ((JArray)resultObject["items"]);
+
+            foreach (JToken entry in itemsArray.Children())
+            {
+                Assert.IsInstanceOfType(entry, typeof(JObject));
+
+                JObject child = (JObject)entry;
+
+                if ((child["type"] != null) && (((string)child["type"]) == "answer"))
+                {
+                    Assert.IsInstanceOfType(child["text"], typeof(JArray));
+
+                    JArray textArray = (JArray)child["text"];
+
+                    Assert.AreEqual("I'm doing pretty well", textArray[0]);
+                    Assert.AreEqual("A different response", textArray[1]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TransformNestedNestedJSONValid()
+        {
+            JToken source = GetJSONObject(@"JSON\Source\nestedNested.json");
+            JToken transform = GetJSONObject(@"JSON\Transform\nestedNested.json");
+
+            JToken result = Transformer.TransformJSON(source, transform);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(JArray));
+
+            JArray resultArray = ((JArray)result);
+
+            Assert.AreEqual(resultArray.Count, 3);
+
+            foreach (JToken entry in resultArray.Children())
+            {
+                Assert.IsInstanceOfType(entry, typeof(JObject));
+
+                JObject child = (JObject)entry;
+
+                if (child["responseType"] != null) Assert.AreEqual("survey", (string)child["responseType"]);
+                else if (child["item"] != null)
+                {
+                    if (((string)child["item"]) == "question") Assert.AreEqual("How are you doing?", (string)child["text"]);
+                    else if (((string)child["item"]) == "answer") Assert.AreEqual("A different response", (string)child["text"]["response_b"]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TransformNestedFailingJSONValid()
+        {
+            JToken source = GetJSONObject(@"JSON\Source\nested.json");
+            JToken transform = GetJSONObject(@"JSON\Transform\nestedFailing.json");
+
+            bool failed = false;
+
+            try
+            {
+                Transformer.TransformJSON(source, transform);
+            }
+            catch (Exception)
+            {
+                failed = true;
+            }
+
+            Assert.IsTrue(failed);
+        }
+
+        [TestMethod]
+        public void TransformNestedConditionalJSONValid()
+        {
+            JToken source = GetJSONObject(@"JSON\Source\nestedNested.json");
+            JToken transform = GetJSONObject(@"JSON\Transform\simpleConditional.json");
+
+            JToken result = Transformer.TransformJSON(source, transform);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(JArray));
+
+            JArray resultArray = ((JArray)result);
+
+            Assert.AreEqual(resultArray.Count, 2);
+
+            bool passingThere = false;
+            bool failingThere = false;
+
+            foreach (JToken entry in resultArray.Children())
+            {
+                Assert.IsInstanceOfType(entry, typeof(JObject));
+
+                JObject child = (JObject)entry;
+
+                if (child["item"] != null)
+                {
+                    if (((string)child["item"]) == "questionPassing")
+                    {
+                        passingThere = true;
+                        Assert.AreEqual("How are you doing?", (string)child["text"]);
+                    }
+                    else if (((string)child["item"]) == "questionFailing") failingThere = true;
+                }
+            }
+
+            Assert.IsTrue(passingThere);
+            Assert.IsFalse(failingThere);
+        }
+
+        private JToken GetJSONObject(string path)
+        {
+            return JToken.Parse(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, path)));
+        }
+    }
+}
---
+++ JSONTransform/JSONTranform.cs
@@ -0,0 +1,254 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace JSONTranform
+{
+    public static class Transformer
+    {
+        private static readonly string _tokenRegex = "{{(.+?)}}";
+
+        #region Methods for transforming JSON
+
+        public static JToken TransformJSON(JToken source, JToken transformation)
+        {
+           // Starting transformation
+
+            JToken returnJSON = null;
+
+            if ((source != null) && (transformation != null))
+            {
+                // Valid source and transformation documents supplied
+
+                if (source.GetType() == typeof(JObject))
+                {
+                    // Source of document determined to be JObject
+
+                    returnJSON = TransformToken((JObject)source, transformation);
+                }
+                else if (source.GetType() == typeof(JArray))
+                {
+                    // Source of document determined to be JArray
+
+                    returnJSON = new JArray();
+
+                    foreach (JToken child in source.Children())
+                    {
+                        if (child.GetType() == typeof(JObject))
+                        {
+                            JToken transformedToken = TransformToken((JObject)source, transformation);
+
+                            if (transformedToken != null) ((JArray)returnJSON).Add(transformedToken);
+                        }
+                    }
+                }
+            }
+
+            return returnJSON;
+        }
+
+        private static JToken TransformToken(JObject source, JToken transformation)
+        {
+            // Parsing transformation token
+
+            JToken returnJSON = null;
+
+            if (transformation.GetType() == typeof(JObject))
+            {
+                // Transformation determined to be JObject
+
+                returnJSON = TransformObject((JObject)source, (JObject)transformation);
+            }
+            else if (transformation.GetType() == typeof(JArray))
+            {
+               // Transformation determined to be JArray
+
+                returnJSON = new JArray();
+
+                foreach (JToken child in transformation.Children())
+                {
+                    if (child.GetType() == typeof(JObject))
+                    {
+                        JObject transformedObject = TransformObject((JObject)source, (JObject)child);
+
+                        if (transformedObject != null) ((JArray)returnJSON).Add(transformedObject);
+                    }
+                }
+            }
+
+            return returnJSON;
+        }
+
+        private static JObject TransformObject(JObject source, JObject transformation)
+        {
+            // Parsing transformation object
+
+            Dictionary<string, object> returnJSON = new Dictionary<string, object>();
+            Dictionary<string, object> serializedTransformation = transformation.ToObject<Dictionary<string, object>>();
+
+            // Checking for transformation condition
+
+            bool hasCondition = false;
+            string condition = String.Empty;
+
+            foreach (string property in serializedTransformation.Keys)
+            {
+                if (property.ToLower() == "==")
+                {
+                    // Transformation condition found
+
+                    hasCondition = true;
+                    condition = TransformString((string)serializedTransformation[property], source).ToString();
+                    break;
+                }
+            }
+
+            if (!hasCondition) returnJSON = BuildResponseJSON(source, serializedTransformation);
+            else
+            {
+                bool isValid = false;
+
+                if (condition.Contains("!="))
+                {
+                    // Transformation condition is NEQ
+
+                    string[] conditionParts = condition.Split(new char[] { '=' });
+
+                    string leftSide = conditionParts[0].TrimEnd(new char[] { '!' }).Replace("'", String.Empty).Replace("\"", String.Empty).Trim();
+                    string rightSide = conditionParts[1].Replace("'", String.Empty).Replace("\"", String.Empty).Trim();
+                    
+                    isValid = leftSide.ToLower() != rightSide.ToLower();
+                }
+                else if (condition.Contains("="))
+                {
+                    // Transformation condition is EQ
+
+                    string[] conditionParts = condition.Split(new char[] { '=' });
+
+                    string leftSide = conditionParts[0].Replace("'", String.Empty).Replace("\"", String.Empty).Trim();
+                    string rightSide = conditionParts[1].Replace("'", String.Empty).Replace("\"", String.Empty).Trim();
+                    
+                    isValid = leftSide.ToLower() == rightSide.ToLower();
+                }
+
+                if (isValid) returnJSON = BuildResponseJSON(source, serializedTransformation);
+                else return null;
+            }
+
+            return JObject.FromObject(returnJSON);
+        }
+
+        private static Dictionary<string, object> BuildResponseJSON(JObject source, Dictionary<string, object> serializedTransformation)
+        {
+            // Building response JSON
+
+            Dictionary<string, object> returnJSON = new Dictionary<string, object>();
+
+            foreach (string property in serializedTransformation.Keys)
+            {
+                if (property.ToLower() != "condition")
+                {
+                    if (serializedTransformation[property].GetType() == typeof(string)) returnJSON.Add(TransformString(property, source).ToString(), TransformString((string)serializedTransformation[property], source));
+                    else if (serializedTransformation[property].GetType() == typeof(JObject)) returnJSON.Add(TransformString(property, source).ToString(), TransformObject(source, (JObject)serializedTransformation[property]));
+                    else if (serializedTransformation[property].GetType() == typeof(JArray)) returnJSON.Add(TransformString(property, source).ToString(), TransformObject(source, (JArray)serializedTransformation[property]));
+                    else returnJSON.Add(TransformString(property, source).ToString(), serializedTransformation[property]);
+                }
+            }
+
+            return returnJSON;
+        }
+
+        private static JArray TransformObject(JObject source, JArray transformation)
+        {
+            List<object> returnJSON = new List<object>();
+
+            foreach (JToken child in transformation.Children())
+            {
+                if (child.Type == JTokenType.String) returnJSON.Add(TransformString((string)child, source));
+                else if (child.GetType() == typeof(JObject)) returnJSON.Add(TransformObject(source, (JObject)child));
+                else returnJSON.Add(child);
+            }
+
+            return JArray.FromObject(returnJSON);
+        }
+
+        private static object TransformString(string target, JObject resource)
+        {
+            // Attempting to transform string
+
+            string returnString = target;
+
+            foreach (Match match in Regex.Matches(target, _tokenRegex))
+            {
+                // Match found
+
+                string cleanedMatch = match.Value.Substring(2);
+                cleanedMatch = cleanedMatch.Substring(0, cleanedMatch.Length - 2);
+
+                string[] matchPath = cleanedMatch.Trim().Split(new char[] { '.' });
+                JToken matchedObject = resource;
+
+                if (matchPath.Length == 1) matchedObject = GetObjectProperty(matchPath[0], matchedObject);
+                else
+                {
+                    foreach (string matchPathPart in matchPath)
+                    {
+                        matchedObject = GetObjectProperty(matchPathPart, matchedObject);
+
+                        if (matchedObject == null) break;
+                    }
+                }
+
+                if (matchedObject != null)
+                {
+                    if (matchedObject.Type == JTokenType.String) returnString = returnString.Replace(match.Value, (string)matchedObject);
+                    else return matchedObject;
+                }
+                else throw new Exception("Unable to match value \"" + match.Value + "\"");
+            }
+
+            return returnString;
+        }
+
+        private static JToken GetObjectProperty(string property, JToken obj)
+        {
+            // Checking to see if an array is specified for matching
+
+            Match arrayMatch = Regex.Match(property, @"\[[(0-9)]\]");
+
+            if (arrayMatch.Success)
+            {
+                // An array match is required
+
+                string propertyNameCleaned = property.Replace(arrayMatch.Value, String.Empty);
+
+                // Checking that the requested property to match on is a array
+
+                if (((JObject)obj)[propertyNameCleaned].GetType() == typeof(JArray))
+                {
+                    // Matched property is array
+
+                    JArray objectArray = (JArray)((JObject)obj)[propertyNameCleaned];
+
+                    string cleanedArrayMatch = arrayMatch.Value.Substring(1);
+                    cleanedArrayMatch = cleanedArrayMatch.Substring(0, cleanedArrayMatch.Length - 1);
+
+                    int arrayValue = Int32.Parse(cleanedArrayMatch);
+
+                    if (arrayValue < objectArray.Count) return objectArray[arrayValue];
+                    else return null;
+                }
+                else
+                {
+                    // Matched property is not an array
+
+                    return null;
+                }
+            }
+            else return ((JObject)obj)[property];
+        }
+
+        #endregion
+    }
+}
